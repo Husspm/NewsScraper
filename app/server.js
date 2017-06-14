9f33a2aac66cf35b8f33138532e92b3af546cadb
Patrick Hussey <husspm@gmail.com>
2017-06-14 16:41:28 -0400

    runnning with this

--- app/server.js
+++ app/server.js
@@ -13,57 +13,76 @@ mongoose.Promise = Promise;
 var app = express();
 
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/NYTscrapper");
 var db = mongoose.connection;
 
-db.on("error", function(error) {
+db.on("error", function (error) {
     console.log("Mongoose Error: ", error);
 });
 
-db.once("open", function() {
+db.once("open", function () {
     console.log("Mongoose connection successful.");
 });
 
-app.get("/scrape", function(req, res) {
-    request("https://www.nytimes.com/section/science?hpw&rref&action=click&pgtype=Homepage&module=well-region&region=bottom-well&WT.nav=bottom-well", function(error, response, html) {
+app.get("/scrape", function (req, res) {
+    request("https://www.nytimes.com/section/science?hpw&rref&action=click&pgtype=Homepage&mo" +
+            "dule=well-region&region=bottom-well&WT.nav=bottom-well",
+    function (error, response, html) {
         var $ = cheerio.load(html);
-        $("div.story-body").each(function(i, element) {
+        $("div.story-body").each(function (i, element) {
 
             // Save an empty result object
             var obj = new Article();
-            // Add the text and href of every link, and save them as properties of the result object
-            var layer2 = $(this).children("a")
+            // Add the text and href of every link, and save them as properties of the
+            // result object
+            var layer2 = $(this)
+                .children("a")
                 .children("div.story-meta");
-            obj.link = $(this).children("a").attr("href");
-            obj.headline = layer2.children("h2.headline").text().trim();
-            obj.summary = layer2.children("p.summary").text();
-            obj.author = layer2.children("p.byline").text();
-            obj.image = $(this).children("a")
+            obj.link = $(this)
+                .children("a")
+                .attr("href");
+            obj.headline = layer2
+                .children("h2.headline")
+                .text()
+                .trim();
+            obj.summary = layer2
+                .children("p.summary")
+                .text();
+            obj.author = layer2
+                .children("p.byline")
+                .text();
+            obj.image = $(this)
+                .children("a")
                 .children("div.wide-thumb")
-                .children("img").attr("src");
+                .children("img")
+                .attr("src");
             Article.create(obj);
 
         });
-        console.log(result);
-
     });
     res.send("Check server console for results");
 });
 
-app.get("/scrape2", function(req, res) {
-    request("https://www.nytimes.com/?action=click&contentCollection=undefined&region=TopBar&module=HomePage-Button&pgtype=sectionfront", function(error, response, html) {
+app.get("/scrape2", function (req, res) {
+    request("https://www.nytimes.com/?action=click&contentCollection=undefined&region=TopBar&" +
+            "module=HomePage-Button&pgtype=sectionfront",
+    function (error, response, html) {
         var $ = cheerio.load(html);
-        $("h2.section-heading").each(function(i, element) {
-            // Add the text and href of every link, and save them as properties of the result object
+        $("h2.section-heading").each(function (i, element) {
+            // Add the text and href of every link, and save them as properties of the
+            // result object
             var obj = new Section();
-            obj.link = $(this).children("a").attr("href");
-            obj.section = $(this).children("a").text().trim();
+            obj.link = $(this)
+                .children("a")
+                .attr("href");
+            obj.section = $(this)
+                .children("a")
+                .text()
+                .trim();
             if (obj.link !== '' && obj.section !== '') {
                 Section.create(obj);
             }
@@ -72,49 +91,61 @@ app.get("/scrape2", function(req, res) {
     res.send("Check server console for results");
 });
 
+app.get("/articles", function (req, res) {
 
-app.get("/articles", function(req, res) {
-
-    Article.find({}, function(error, result) {
-        if (error) {
-            res.json(error);
-        } else {
-            res.send(result);
-        }
-    });
+    Article
+        .find({}, function (error, result) {
+            if (error) {
+                res.json(error);
+            } else {
+                res.send(result);
+            }
+        });
 });
-app.get("/sections", function(req, res) {
+app.get("/sections", function (req, res) {
 
-    Section.find({}, function(error, result) {
-        if (error) {
-            res.json(error);
-        } else {
-            res.send(result);
-        }
-    });
-});
-
-app.get("/articles/:id", function(req, res) {
-
-    Article.findOne({ "_id": req.params.id }).populate("comment").exec(function(err, result) {
-        if (err) {
-            res.send(err);
-        } else {
-            console.log(result);
-            res.send(result);
-        }
-    });
-
+    Section
+        .find({}, function (error, result) {
+            if (error) {
+                res.json(error);
+            } else {
+                res.send(result);
+            }
+        });
 });
 
-app.post("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
+
+    Article
+        .findOne({"_id": req.params.id})
+        .populate("comment")
+        .exec(function (err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                console.log(result);
+                res.send(result);
+            }
+        });
+
+});
+
+app.post("/articles/:id", function (req, res) {
     var newComment = new Comment(req.body);
-    newComment.save(function(error, doc) {
+    newComment.save(function (error, doc) {
         if (error) {
             res.send(error);
         } else {
-            Article.findOneAndUpdate({ "_id": req.params.id }, { $push: { "comment": doc._id } }, { new: true },
-                function(err, data) {
+            Article
+                .findOneAndUpdate({
+                    "_id": req.params.id
+                }, {
+                    $push: {
+                        "comment": doc._id
+                    }
+                }, {
+                    new: true
+                }, function (err, data) {
                     if (err) {
                         res.send(err);
                     } else {
@@ -126,6 +157,6 @@ app.post("/articles/:id", function(req, res) {
 
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("App running on port 3000!");
 });
